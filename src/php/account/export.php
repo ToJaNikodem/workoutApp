ddc4093d0e3afe96291d3fd766a87f62d33933e7
ToJaNikodem <nikodembuczkowski05@gmail.com>
2023-06-17 15:07:17 +0200

    made export.php better

--- src/php/account/export.php
+++ src/php/account/export.php
@@ -34,177 +34,7 @@ $stmt->store_result();
 $stmt->bind_result($workoutId);
 
 while ($stmt->fetch()) {
-    if (!$stmt2 = $conn->prepare($workoutDataQuery)) {
-        mainPageHeader($databaseError);
-        exit;
-    }
-
-    $stmt2->bind_param("s", $workoutId);
-    $stmt2->execute();
-    $stmt2->store_result();
-    $stmt2->bind_result($workoutId2, $defaultWorkoutNameEN, $defaultWorkoutNamePL, $userWorkoutName);
-
-    while ($stmt2->fetch()) {
-        $workout = array(
-            "workoutId" => $workoutId2,
-            "defaultWorkoutNameEN" => $defaultWorkoutNameEN,
-            "defaultWorkoutNamePL" => $defaultWorkoutNamePL,
-            "userWorkoutName" => $userWorkoutName
-        );
-
-        $workouts[] = $workout;
-
-        $data .= addWorkouts($workouts);
-        unset($workouts);
-
-        if (!$stmt3 = $conn->prepare($workoutVariantDataQuery)) {
-            mainPageHeader($databaseError);
-            exit;
-        }
-
-        $stmt3->bind_param("s", $workoutId);
-        $stmt3->execute();
-        $stmt3->store_result();
-        $stmt3->bind_result($workoutVariantId, $defaultWorkoutVariantNameEN, $defaultWorkoutVariantNamePL, $userWorkoutVariantName, $workoutVariantNotes);
-
-        while ($stmt3->fetch()) {
-            $workoutVariant = array(
-                "workoutVariantId" => $workoutVariantId,
-                "defaultWorkoutVariantNameEN" => $defaultWorkoutVariantNameEN,
-                "defaultWorkoutVariantNamePL" => $defaultWorkoutVariantNamePL,
-                "userWorkoutVariantName" => $userWorkoutVariantName,
-                "workoutVariantNotes" => $workoutVariantNotes
-            );
-
-            $workoutVariants[] = $workoutVariant;
-            $strengthExercises = array();
-            $cardioExercises = array();
-            $otherExercises = array();
-            $sets = array();
-
-            $data .= addWorkoutVariants($workoutVariants);
-            unset($workoutVariants);
-
-
-            if (!$stmt4 = $conn->prepare($strengthExerciseDataQuery)) {
-                mainPageHeader($databaseError);
-                exit;
-            }
-
-            $stmt4->bind_param("s", $workoutVariantId);
-            $stmt4->execute();
-            $stmt4->store_result();
-            $stmt4->bind_result($strengthExerciseId, $strengthExerciseNameEN, $strengthExerciseNamePL, $userStrengthExerciseName, $strengthExerciseNotes, $strengthExerciseOrder);
-
-            while ($stmt4->fetch()) {
-                $strengthExercise = array(
-                    "strengthExerciseId" => $strengthExerciseId,
-                    "exerciseNameEN" => $strengthExerciseNameEN,
-                    "exerciseNamePL" => $strengthExerciseNamePL,
-                    "userExerciseName" => $userStrengthExerciseName,
-                    "exerciseNotes" => $strengthExerciseNotes,
-                    "isStrengthExercise" => 1,
-                    "exerciseOrder" => $strengthExerciseOrder
-                );
-
-                $strengthExercises[] = $strengthExercise;
-
-
-                if (!$stmt7 = $conn->prepare($setsDataQuery)) {
-                    mainPageHeader($databaseError);
-                    exit;
-                }
-
-                $stmt7->bind_param("s", $strengthExerciseId);
-                $stmt7->execute();
-                $stmt7->store_result();
-                $stmt7->bind_result($setStrengthExerciseId, $setNumber, $repCount, $weight, $setNotes, $dropset);
-
-                while ($stmt7->fetch()) {
-                    $set = array(
-                        "exerciseId" => $strengthExerciseId,
-                        "setNumber" => $setNumber,
-                        "repCount" => $repCount,
-                        "weight" => $weight,
-                        "setNotes" => $setNotes,
-                        "dropset" => $dropset,
-                    );
-                    $sets[] = $set;
-
-                    usort($sets, function ($a, $b) {
-                        $orderA = $a["setNumber"] ?? PHP_INT_MAX;
-                        $orderB = $b["setNumber"] ?? PHP_INT_MAX;
-
-                        return $orderA - $orderB;
-                    });
-                }
-            }
-
-            if (!$stmt5 = $conn->prepare($cardioExerciseDataQuery)) {
-                mainPageHeader($databaseError);
-                exit;
-            }
-
-            $stmt5->bind_param("s", $workoutVariantId);
-            $stmt5->execute();
-            $stmt5->store_result();
-            $stmt5->bind_result($cardioExerciseId, $cardioExerciseNameEN, $cardioExerciseNamePL, $userCardioExerciseName, $duration, $distance, $speed, $cardioExerciseNotes, $cardioExerciseOrder);
-
-            while ($stmt5->fetch()) {
-                $cardioExercise = array(
-                    "exerciseId" => $cardioExerciseId,
-                    "exerciseNameEN" => $cardioExerciseNameEN,
-                    "exerciseNamePL" => $cardioExerciseNamePL,
-                    "userExerciseName" => $userCardioExerciseName,
-                    "duration" => $duration,
-                    "distance" => $distance,
-                    "speed" => $speed,
-                    "exerciseNotes" => $cardioExerciseNotes,
-                    "exerciseOrder" => $cardioExerciseOrder
-                );
-                $cardioExercises[] = $cardioExercise;
-            }
-
-            if (!$stmt6 = $conn->prepare($otherExerciseDataQuery)) {
-                mainPageHeader($databaseError);
-            }
-
-            $stmt6->bind_param("s", $workoutVariantId);
-            $stmt6->execute();
-            $stmt6->store_result();
-            $stmt6->bind_result($otherExerciseId, $otherExerciseNameEN, $otherExerciseNamePL, $userOtherExerciseName, $otherExerciseNotes, $otherExerciseOrder);
-
-            while ($stmt6->fetch()) {
-                $otherExercise = array(
-                    "exerciseId" => $otherExerciseId,
-                    "exerciseNameEN" => $otherExerciseNameEN,
-                    "exerciseNamePL" => $otherExerciseNamePL,
-                    "userExerciseName" => $userOtherExerciseName,
-                    "exerciseNotes" => $otherExerciseNotes,
-                    "exerciseOrder" => $otherExerciseOrder
-                );
-                $otherExercises[] = $otherExercise;
-            }
-
-            $exercises = array_merge($strengthExercises, $cardioExercises, $otherExercises);
-
-            unset($strengthExercises);
-            unset($cardioExercises);
-            unset($otherExercises);
-
-            usort($exercises, function ($a, $b) {
-                $orderA = $a["exerciseOrder"] ?? PHP_INT_MAX;
-                $orderB = $b["exerciseOrder"] ?? PHP_INT_MAX;
-
-                return $orderA - $orderB;
-            });
-
-            $data .= addExercises($exercises, $sets);
-            unset($sets);
-            unset($exercises);
-            $data .= "\n\n";
-        }
-    }
+    $data .= fetchWorkouts($conn, $workoutId);
 }
 
 function addWorkouts($workouts) {
@@ -281,18 +111,252 @@ function addExercises($exercises, $sets) {
 function addSets($sets, $exercise) {
     $data = '';
     foreach ($sets as $set) {
-        if (!empty($exercise["strengthExerciseId"])) {
-            if ($set["exerciseId"] == $exercise["strengthExerciseId"] && $exercise["isStrengthExercise"] == 1) {
-                $data .= "         ";
-                $data .= "set " . $set["setNumber"] . ". reps: " . $set["repCount"] . "; weight: " . $set["weight"] . "; ";
-                if ($set["dropset"] == 1) {
-                    $data .= "dropset ";
-                }
-                $data .= "   Notes: " . $set["setNotes"] . "\n";
-            }
+        if (empty($exercise["strengthExerciseId"])) {
+            continue;
         }
+        
+        if ($set["exerciseId"] != $exercise["strengthExerciseId"] || $exercise["isStrengthExercise"] != 1) {
+            continue;
+        }
+        
+        $data .= "         ";
+        $data .= "set " . $set["setNumber"] . ". reps: " . $set["repCount"] . "; weight: " . $set["weight"] . "; ";
+        
+        if ($set["dropset"] == 1) {
+            $data .= "   dropset ";
+        }
+        
+        if (!empty($set['setNotes'])) {
+            $data .= "   Notes: " . $set["setNotes"];
+        }
+        
+        $data .= "\n";
     }
     return $data;
+}
+
+function fetchWorkouts($conn, $workoutId) {
+    $data = '';
+    global $workoutDataQuery;
+    global $databaseError;
+    if (!$stmt2 = $conn->prepare($workoutDataQuery)) {
+        mainPageHeader($databaseError);
+        exit;
+    }
+
+    $workoutId2 = 0; $defaultWorkoutNameEN = ''; $defaultWorkoutNamePL = ''; $userWorkoutName = '';
+
+    $stmt2->bind_param("s", $workoutId);
+    $stmt2->execute();
+    $stmt2->store_result();
+    $stmt2->bind_result($workoutId2, $defaultWorkoutNameEN, $defaultWorkoutNamePL, $userWorkoutName);
+
+    while ($stmt2->fetch()) {
+        $workout = array(
+            "workoutId" => $workoutId2,
+            "defaultWorkoutNameEN" => $defaultWorkoutNameEN,
+            "defaultWorkoutNamePL" => $defaultWorkoutNamePL,
+            "userWorkoutName" => $userWorkoutName
+        );
+        $workouts[] = $workout;
+
+        $data .= addWorkouts($workouts);
+        unset($workouts);
+
+        $data .= fetchWorkoutVariants($conn, $workoutId);
+    }
+    return $data;
+}
+
+function fetchWorkoutVariants($conn, $workoutId) {
+    $data = '';
+    global $workoutVariantDataQuery;
+    global $databaseError;
+    if (!$stmt3 = $conn->prepare($workoutVariantDataQuery)) {
+        mainPageHeader($databaseError);
+        exit;
+    }
+
+    $workoutVariantId = 0; $defaultWorkoutVariantNameEN = ''; $defaultWorkoutVariantNamePL = ''; $userWorkoutVariantName = ''; $workoutVariantNotes = '';
+
+    $stmt3->bind_param("s", $workoutId);
+    $stmt3->execute();
+    $stmt3->store_result();
+    $stmt3->bind_result($workoutVariantId, $defaultWorkoutVariantNameEN, $defaultWorkoutVariantNamePL, $userWorkoutVariantName, $workoutVariantNotes);
+
+    while ($stmt3->fetch()) {
+        $workoutVariant = array(
+            "workoutVariantId" => $workoutVariantId,
+            "defaultWorkoutVariantNameEN" => $defaultWorkoutVariantNameEN,
+            "defaultWorkoutVariantNamePL" => $defaultWorkoutVariantNamePL,
+            "userWorkoutVariantName" => $userWorkoutVariantName,
+            "workoutVariantNotes" => $workoutVariantNotes
+        );
+
+        $workoutVariants[] = $workoutVariant;
+        
+        $data .= addWorkoutVariants($workoutVariants);
+        unset($workoutVariants);
+
+        [$strengthExercises, $sets] = fetchStrengthExercises($conn, $workoutVariantId);
+        $cardioExercises = fetchCardioExercises($conn, $workoutVariantId);
+        $otherExercises = fetchOtherExercises($conn, $workoutVariantId);
+
+        $exercises = array_merge($strengthExercises, $cardioExercises, $otherExercises);
+
+        unset($strengthExercises);
+        unset($cardioExercises);
+        unset($otherExercises);
+
+        usort($exercises, function ($a, $b) {
+            $orderA = $a["exerciseOrder"] ?? PHP_INT_MAX;
+            $orderB = $b["exerciseOrder"] ?? PHP_INT_MAX;
+
+            return $orderA - $orderB;
+        });
+
+        $data .= addExercises($exercises, $sets);
+        unset($sets);
+        unset($exercises);
+        $data .= "\n\n";
+    }
+    return $data;
+}
+
+function fetchStrengthExercises($conn, $workoutVariantId) {
+    $sets = array();
+    $strengthExercises = array();
+    global $databaseError;
+    global $strengthExerciseDataQuery;
+    if (!$stmt4 = $conn->prepare($strengthExerciseDataQuery)) {
+        mainPageHeader($databaseError);
+        exit;
+    }
+
+    $strengthExerciseId = 0; $strengthExerciseNameEN = ''; $strengthExerciseNamePL = ''; $userStrengthExerciseName = ''; $strengthExerciseNotes = ''; $strengthExerciseOrder = 0;
+
+    $stmt4->bind_param("s", $workoutVariantId);
+    $stmt4->execute();
+    $stmt4->store_result();
+    $stmt4->bind_result($strengthExerciseId, $strengthExerciseNameEN, $strengthExerciseNamePL, $userStrengthExerciseName, $strengthExerciseNotes, $strengthExerciseOrder);
+
+    while ($stmt4->fetch()) {
+        $strengthExercise = array(
+            "strengthExerciseId" => $strengthExerciseId,
+            "exerciseNameEN" => $strengthExerciseNameEN,
+            "exerciseNamePL" => $strengthExerciseNamePL,
+            "userExerciseName" => $userStrengthExerciseName,
+            "exerciseNotes" => $strengthExerciseNotes,
+            "isStrengthExercise" => 1,
+            "exerciseOrder" => $strengthExerciseOrder
+        );
+
+        $strengthExercises[] = $strengthExercise;
+
+        $exerciseSets = fetchSets($conn, $strengthExerciseId);
+        $sets = array_merge($sets, $exerciseSets);
+    }
+    return [$strengthExercises, $sets];
+}
+
+function fetchSets($conn, $strengthExerciseId) {
+    $sets = array();
+    global $databaseError;
+    global $setsDataQuery;
+    if (!$stmt7 = $conn->prepare($setsDataQuery)) {
+        mainPageHeader($databaseError);
+        exit;
+    }
+
+    $setStrengthExerciseId = 0; $setNumber = 0; $repCount = 0; $weight = 0; $setNotes = ''; $dropset = 0;
+
+    $stmt7->bind_param("s", $strengthExerciseId);
+    $stmt7->execute();
+    $stmt7->store_result();
+    $stmt7->bind_result($setStrengthExerciseId, $setNumber, $repCount, $weight, $setNotes, $dropset);
+
+    while ($stmt7->fetch()) {
+        $set = array(
+            "exerciseId" => $strengthExerciseId,
+            "setNumber" => $setNumber,
+            "repCount" => $repCount,
+            "weight" => $weight,
+            "setNotes" => $setNotes,
+            "dropset" => $dropset,
+        );
+        $sets[] = $set;
+
+        usort($sets, function ($a, $b) {
+            $orderA = $a["setNumber"] ?? PHP_INT_MAX;
+            $orderB = $b["setNumber"] ?? PHP_INT_MAX;
+
+            return $orderA - $orderB;
+        });
+    }
+    return $sets;
+}
+
+function fetchCardioExercises($conn, $workoutVariantId) {
+    $cardioExercises = array();
+    global $databaseError;
+    global $cardioExerciseDataQuery;
+    if (!$stmt5 = $conn->prepare($cardioExerciseDataQuery)) {
+        mainPageHeader($databaseError);
+        exit;
+    }
+
+    $cardioExerciseId = 0; $cardioExerciseNameEN = ''; $cardioExerciseNamePL = ''; $userCardioExerciseName = ''; 
+    $duration = 0; $distance = 0; $speed = 0; $cardioExerciseNotes = ''; $cardioExerciseOrder = 0;
+
+    $stmt5->bind_param("s", $workoutVariantId);
+    $stmt5->execute();
+    $stmt5->store_result();
+    $stmt5->bind_result($cardioExerciseId, $cardioExerciseNameEN, $cardioExerciseNamePL, $userCardioExerciseName, $duration, $distance, $speed, $cardioExerciseNotes, $cardioExerciseOrder);
+
+    while ($stmt5->fetch()) {
+        $cardioExercise = array(
+            "exerciseId" => $cardioExerciseId,
+            "exerciseNameEN" => $cardioExerciseNameEN,
+            "exerciseNamePL" => $cardioExerciseNamePL,
+            "userExerciseName" => $userCardioExerciseName,
+            "duration" => $duration,
+            "distance" => $distance,
+            "speed" => $speed,
+            "exerciseNotes" => $cardioExerciseNotes,
+            "exerciseOrder" => $cardioExerciseOrder
+        );
+        $cardioExercises[] = $cardioExercise;
+    }
+    return $cardioExercises;
+}
+
+function fetchOtherExercises($conn, $workoutVariantId) {
+    $otherExercises = array();
+    global $databaseError;
+    global $otherExerciseDataQuery;
+    if (!$stmt6 = $conn->prepare($otherExerciseDataQuery)) {
+        mainPageHeader($databaseError);
+    }
+
+    $otherExerciseId = 0; $otherExerciseNameEN = ''; $otherExerciseNamePL = ''; $userOtherExerciseName = ''; $otherExerciseNotes = ''; $otherExerciseOrder = 0;
+
+    $stmt6->bind_param("s", $workoutVariantId);
+    $stmt6->execute();
+    $stmt6->store_result();
+    $stmt6->bind_result($otherExerciseId, $otherExerciseNameEN, $otherExerciseNamePL, $userOtherExerciseName, $otherExerciseNotes, $otherExerciseOrder);
+
+    while ($stmt6->fetch()) {
+        $otherExercise = array(
+            "exerciseId" => $otherExerciseId,
+            "exerciseNameEN" => $otherExerciseNameEN,
+            "exerciseNamePL" => $otherExerciseNamePL,
+            "userExerciseName" => $userOtherExerciseName,
+            "exerciseNotes" => $otherExerciseNotes,
+            "exerciseOrder" => $otherExerciseOrder
+        );
+        $otherExercises[] = $otherExercise;
+    }
+    return $otherExercises;
 }
 
 $conn->close();
